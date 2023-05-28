637e9ada47adf84ec628268cc55455935226af19
Silver-Eun <h22sang@naver.com>
2023-05-28 17:56:44 +0900

    Moviepedia2
    sort
    filter
    key Prop
    fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
+import { getReviews } from "../api";
 import ReviewList from "./ReviewList";
-import items from "../mock.json";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [order, setOrder] = useState(["createdAt"]);
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews);
+  };
+
   return (
     <div>
-      <ReviewList items={items} />
+      <button onClick={handleNewestClick}>Newest</button>
+      <button onClick={handleBestClick}>Best</button>
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>Load</button>
     </div>
   );
 }
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -3,7 +3,9 @@ function formatDate(value) {
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
 
-function ReviewListItem({ item }) {
+function ReviewListItem({ item, onDelte }) {
+  const handleDeleteClick = () => onDelte(item.id);
+
   return (
     <div className="ReviewListItem">
       <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title}></img>
@@ -12,18 +14,19 @@ function ReviewListItem({ item }) {
         <p>{item.rating}</p>
         <p>{formatDate(item.createdAt)}</p>
         <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>Delete</button>
       </div>
     </div>
   );
 }
 
-function ReviewList({ items }) {
+function ReviewList({ items, onDelete }) {
   return (
     <ul>
       {items.map((item) => {
         return (
-          <li>
-            <ReviewListItem item={item} />
+          <li key={item.id}>
+            <ReviewListItem item={item} onDelte={onDelete} />
           </li>
         );
       })}
