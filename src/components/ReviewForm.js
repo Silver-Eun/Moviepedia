77168e29c9766b86ae686098f28a7fd1e98f0f45
Silver-Eun <h22sang@naver.com>
2023-06-03 13:40:57 +0900

    Moviepedia6

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,18 +1,24 @@
 import { useState } from "react";
+import FileInput from "./FileInput";
 
 function ReviewForm() {
   const [values, setValues] = useState({
     title: "",
     rating: 0,
     content: "",
+    imgFile: null,
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleChange = (name, value) => {
     setValues((preValues) => ({
       ...preValues,
       [name]: value,
     }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
   };
 
   const handleSubmit = (e) => {
@@ -21,6 +27,7 @@ function ReviewForm() {
   };
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
+      <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
       <input name="title" value={values.title} onChange={handleChange} />
       <input type="number" value={values.rating} onChange={handleChange} />
       <textarea name="content" value={values.content} onChange={handleChange} />
